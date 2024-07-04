f4cd91d51b2a37a2670cc84e9c9f22ac69450932
CandL <ce.slater@yahoo.com>
2024-07-04 10:13:58 -0400

    Cleaned up main.py
    Simpler code

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ import FreeCAD
 
 import Path
 
-print("Available functions in PAth")
+print("\n\nAvailable functions in PAth")
 print(getmembers(Path, isfunction))
 txt=dir(Path)
 print("txt:",txt)
@@ -19,7 +19,7 @@ print ("::::::::::::::::::::::::::::::::")
 
 import PathScripts
 #from PathScripts import PathUtils as pu
-print("Available functions in PAthScrits")
+print("\n\nAvailable functions in PAthScrits")
 print(getmembers(Path, isfunction))
 txt1=dir(PathScripts)
 print("txt:",txt1)
@@ -28,7 +28,7 @@ print("Done ....Available functions in PAthScrits")
 print ("::::::::::::::::::::::::::::::::")
 
 import JobUtils
-print("Available functions in JobUtils")
+print("\n\nAvailable functions in JobUtils")
 print(getmembers(JobUtils, isfunction))
 txt2=dir(JobUtils)
 print("txt:",txt2)
@@ -63,363 +63,126 @@ filepath = "test1.FCStd"
 doc=FreeCAD.open(filepath)
 
 print(type(doc))
-print(doc.Label)
+print("Doc Label:",doc.Label)
 
+print("These are global app settings:")
+print("\t- Available lib paths for tools",JobUtils._get_available_tool_library_paths())
 
-print("Available lib paths for tools")
-toolDirs=JobUtils._get_available_tool_library_paths()
-print(toolDirs)
 
 # HARD CODED FOR NOW
-print("toolDirs[0][2]", toolDirs[0][2])
+toolDirs=JobUtils._get_available_tool_library_paths()
+print("\t- toolDirs[0][2]", toolDirs[0][2])
 toolList=JobUtils._read_library(toolDirs[0][2])
-print(toolList)
-
-print("Available FreeCAD.ActiveDocument")
-print(dir(FreeCAD.ActiveDocument))
-
-
-print("Jobs")
-newJob = FreeCAD.ActiveDocument.getObject("Job")
-print(type(newJob))
-print(newJob.Label)
+print ("\t- toolList: ")
+for aTool in toolList:
+    print("\t\t:",aTool)
 
 
-print("Operations")
-p = FreeCAD.ActiveDocument.getObject("Operations")
-print(type(p))
-
-print("dressups")
-p = FreeCAD.ActiveDocument.getObject("DressupTag")
-print(type(p))
-
-
-
-# poster=Path.Post.CommandPathPost()
-
-print("()()()()()())()()()()()()")
+print("\n\n()()()()()())()()()()()()\n\n")
 
 docObjs=doc.Objects
-print(type(docObjs))
-
-for obj in docObjs:
-    print("Object Name: ", obj.Name, "  | Object Label: ", obj.Label," | Object Type: ", obj.TypeId )
-# print("Object Name: ", obj.Name , "Object Label: ", obj.Label, "Object Type: ", obj.TypeId, "Object SubType: ", obj.SubTypeId)
-
-print("()-()-()-()-()-()-()-()-()-()-()-()")
-
-for obj in docObjs:
-    if obj.TypeId == 'Path::FeaturePython':
-        print("Object Name: ", obj.Name, "  | Object Label: ", obj.Label," | Object Type: ", obj.TypeId )
+#print(type(docObjs))
 
 
 opArray=[]
 toolArray=[]
 jobArray=[]
 
+print("Parsing out CAM objects:")
 for obj in docObjs:
-    if obj.TypeId == 'Path::FeaturePython':
-        if "PROFILE" in obj.Name.upper() or "POCKET" in obj.Name.upper() or \
-            "DRILL" in obj.Name.upper() or "FACE" in obj.Name.upper() or \
-            "CONTOUR" in obj.Name.upper() or "THREAD" in obj.Name.upper() or \
-            "ENGRAVE" in obj.Name.upper() or "DEBURR" in obj.Name.upper():
-            opArray.append(obj)
 
-        if "TC__" in obj.Name.upper():
-            toolArray.append(obj)
+    match obj.TypeId:
+
+        case "App::FeaturePython":
+            pass      
+
+        case "App::DocumentObjectGroup":
+            pass
+
+        case "App::Line":
+            pass
+
+        case "App::Origin":
+            pass
+
+        case "App::Plane":
+            pass
+
+        case "Part::FeaturePython":
+            pass
+
+        case "PartDesign::Body":
+            pass
+
+        case "PartDesign::Pad":
+            pass
+
+        case "PartDesign::Pocket":
+            pass
+
+        case "Sketcher::SketchObject":            
+            pass
+
+        case "Path::FeaturePython":
+            #print("Processing Path::FeaturePython")
+            typeAsString=str(type(obj.Proxy))
+            #print("typeAsString: ", typeAsString)
+
+            if "Path.Main.Job" in typeAsString:
+                jobArray.append(obj)    
+
+            if "Path.Tool" in typeAsString:
+                toolArray.append(obj)
+
+            if "Path.Op" in typeAsString:
+                opArray.append(obj)
+
+        case _:
+            print("Don't know this object type",obj.TypeId)
+
+print("\tjobArray size: ",len(jobArray))
+print("\ttoolArray size: ",len(toolArray))
+print("\topArray size: ",len(opArray))
+
+
+if len(jobArray) > 0:
+    print("\n\nPrinting JobArray[0]:")
+    #print(dir(jobArray[0]))
+    print("\tLabel:",jobArray[0].Label)
+    print("\tFixtures:",jobArray[0].Fixtures)
+    print("\tPostProcessor:",jobArray[0].PostProcessor)
+    print("\tPostProcessorArgs:",jobArray[0].PostProcessorArgs)
+    print("\tPostProcessorOutputFile:",jobArray[0].PostProcessorOutputFile)
+    print("\tSplitOutput:",jobArray[0].SplitOutput)
+
+if len(toolArray) > 0:
+    print("\n\nPrinting ToolArray[0]:")
+    #print(dir(toolArray[0]))
+    print("\tLabel:",toolArray[0].Label)
+    print("\tToolNumber:",toolArray[0].ToolNumber)
+    print("\tSpindleSpeed:",toolArray[0].SpindleSpeed)
+    print("\t:SpindleDir",toolArray[0].SpindleDir)
+    print("\tHorizFeed:",toolArray[0].HorizFeed)
+    print("\tVertFeed:",toolArray[0].VertFeed)
+    
         
-        if "JOB" in obj.Name.upper():
-            jobArray.append(obj)
-            
-print("\n\n")
-print("opArray size: ", len(opArray))
-for op in opArray:
-    print("op.Name: ", op.Name, " | op.Label: ", op.Label, " | op.TypeId: ", op.TypeId)
-    print(dir(op))
-    if hasattr(op, 'Label'):
-        print("\tLabel: ", op.Label)
-    if hasattr(op, 'FullName'):
-        print("\tFullName: ", op.FullName)
-    if hasattr(op, 'Active'):
-        print("\tActive: ", op.Active)
-    if hasattr(op, 'Direction'):
-        print("\tDirection: ", op.Direction)
-    else:
-        print("\tDirection: None")
-    if hasattr(op, 'CoolantMode'):
-        print("\tCoolantMode: ", op.CoolantMode) 
-    if hasattr(op, 'CycleTime'):
-        print("\tCycleTime: ", op.CycleTime)
-    if hasattr(op, 'Description'):  
-        print("\tDescription: ", op.Description)
-    if hasattr(op, 'Document'):
-        print("\tDocument: ", op.Document)
-    #if hasattr(op, 'ExpressionEngine'):
-    #    print("\tExpressionEngine: ", op.ExpressionEngine)  
-    if hasattr(op, 'GeometryTolerance'):
-        print("\tGeometryTolerance: ", op.GeometryTolerance)
-    if hasattr(op, 'Group'):
-        print("\tGroup: ", op.Group)
-    if hasattr(op, 'ToolController') :
-        print("\tToolController: ", op.ToolController)
-        print("\tdir ToolController: ", dir(op.ToolController))
-        print("\tToolController.ToolNumber: ", op.ToolController.ToolNumber)
-        print("\tToolController.Tool: ", op.ToolController.Tool)
+if len(opArray) > 0:
+    print("\n\nPrinting OpArray[0]:")
+    print("\tLabel:",opArray[0].Label)
+    print(dir(opArray[0]))
+    print("\tActive:",opArray[0].Active)
+    print("\tState:",opArray[0].State)
+    print("\tClearanceHeight:",opArray[0].ClearanceHeight)
+    print("\tCycleTime:",opArray[0].CycleTime)
+    print("\tDirection:",opArray[0].Direction)
+    print("\tFinalDepth:",opArray[0].FinalDepth)
+    print("\tParents:",opArray[0].Parents)
+    print("\tCoolantMode:",opArray[0].CoolantMode)
 
-    if hasattr(op, 'Path'):
-        print("\tPath: ", op.Path)
-        #print("dir:", dir(op.Path))
-        for cmd in op.Path.Commands:
-            print("\t\tCommands:", cmd)
+    #print(dir(opArray[0].Parents))
+    #print("\t:",opArray[0].)
+    #print("\t:",opArray[0].)
 
-    print("&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&\n")
-                  #print("\tPath cnt: ", op.Path.count)
-        #for aMove in op.Path:
-        #    print("\t\tPseudo Gcode: ", aMove)  
-
-    if hasattr(op, 'Content'):
-        print("\tContent: ", op.Content)
-
-
-print("\n\n")
-print("toolArray size: ",len(toolArray)) 
-for tool in toolArray:
-    print("tool.Name: ", tool.Name, " | tool.Label: ", tool.Label, " | tool.TypeId: ", tool.TypeId)
-    #print(dir(tool))
-    print("tool:",tool.ToolNumber)
-    #  " | ", tool.Name, " | ", tool.Label, " | ", tool.TypeId))
-
-print("\n\n")
-print("jobArray size: ",len(jobArray))  
-for job in jobArray:
-    print("job.Name: ", job.Name, " | job.Label: ", job.Label, " | job.TypeId: ", job.TypeId)
-
-
-
-print("\n\n")
-
-jobObjs=[]
-
-# The attribute Fixtures is unique to Jobs
-for obj in docObjs:
-    if hasattr(obj, 'Fixtures'):
-        if obj.TypeId == 'Path::FeaturePython':
-#            if "Job" in obj.Label:
-            jobObjs.append(obj)
-    
-print("jobObjs: ", jobObjs)
-
-for aJob in jobObjs:
-    print("aJob.Label: ", aJob.Label)
-    print(type(aJob))
-    print(dir(aJob))
-    print("\n\n")
-    print("--------------")
-    print("\n\n")
-    
-
-    # print("Content: ", aJob.Content) gives a list of printable objects
-
-    print("Attributes of a job:")
-    print("   Label: ", aJob.Label)
-    print("   Description: ", aJob.Description)
-    print("   Document: ", aJob.Document)
-    print("   Output filename:",aJob.PostProcessorOutputFile)
-    print("   State: ", aJob.State)
-
-    print("   PostProcessor: ", aJob.PostProcessor)
-    print("   PostProcessor Args: ", aJob.PostProcessorArgs)
-
-    print("   CycleTime: ", aJob.CycleTime)
-    print("   Fixtures: ", aJob.Fixtures)
-    #print("   Job Type: ", aJob.)
-    print("\n")
-
-    print("   SetUp Sheet:")
-    #print("       : ", aJob.SetupSheet.)
-    print("       FullName: ", aJob.SetupSheet.FullName)
-    print("       ClearanceHeightExpression: ", aJob.SetupSheet.ClearanceHeightExpression)
-    print("       ClearanceHeightOffset: ", aJob.SetupSheet.ClearanceHeightOffset)
-    # printing contents lets you know what is in the entire list
-    #print("       Content: ", aJob.SetupSheet.Content)
-    print("       CoolantMode: ", aJob.SetupSheet.CoolantMode)
-    print("       CoolantModes: ", aJob.SetupSheet.CoolantModes)
-    print("       ExpressionEngine: ", aJob.SetupSheet.ExpressionEngine)
-    #print("       Document: ", aJob.SetupSheet.Document)
-    print("       FinalDepthExpression: ", aJob.SetupSheet.FinalDepthExpression)    
-    print("\n")
-
-
-    print("    Tools used:")
-    for tc in aJob.Tools.Group:
-        print("       tc: ", tc.ToolNumber, " name: ", tc.FullName)
-
-    print("\n")
-
-
-
-    print("   Operations:")
-    
-    print("       InList: ")
-    #print("           Content: ", aJob.Operations.InList.Content)
-    #print("           Content: ", aJob.Operations.InList.Group)
-          
-    #lclList=aJob.Operations.InList
-    
-    
-    print("          List size: ", aJob.Operations.InList.__len__())
-    for opListItem in aJob.Operations.InList:
-        print("           opListItem: ", opListItem.Name, " | ", opListItem.Label, " | ", opListItem.TypeId,  " | ", opListItem.FullName)
-        print(dir(opListItem))
-        print(dir(opListItem.Operations))
-        print("name:", opListItem.Operations.Name)
-
-    print("\n")     
-
-    print("       OutList: ")
-    print("          List size: ", aJob.Operations.OutList.__len__())
-    for opListItem in aJob.Operations.OutList:
-        print("           opListItem: ", opListItem.Name, " | ", opListItem.Label, " | ", opListItem.TypeId,  " | ", opListItem.FullName)
-
-
-    print("-----------------")
-
-    #exit()
-
-    type(aJob.Content)
-    print("Content: ", aJob.Content)
-
-    print("============================")
-
-    print("CycleTime: ", aJob.CycleTime)
-    print("Description: ", aJob.Description)
-    print("Document: ", aJob.Document)
-    print("ExpressionEngine: ", aJob.ExpressionEngine)
-
-    
-    print("FullName: ", aJob.FullName)
-    print("GeometryTolerance: ", aJob.GeometryTolerance)
-    print("Group: ", aJob.Group) 
-    print("ID: ", aJob.ID)
-    print("InList: ", aJob.InList)
-    print("InListRecursive: ", aJob.InListRecursive)
-    
-    print("Label2: ", aJob.Label2)
-    print("LastPostProcessDate: ", aJob.LastPostProcessDate)
-    print("LastPostProcessOutput: ", aJob.LastPostProcessOutput)
-    print("MemSize: ", aJob.MemSize)
-    print("Model: ", aJob.Model)
-    print("MustExecute: ", aJob.MustExecute)
-    print("Name: ", aJob.Name)
-    print("NoTouch: ", aJob.NoTouch)
-    print("OldLabel: ", aJob.OldLabel)
-    print("Operations: ", aJob.Operations)
-    print(type(aJob.Operations))
-
-
-    print("dir operation:",dir(aJob.Operations))
-    print("Operations as a class: ", aJob.Operations.__class__)
-    print("Operations as a name: ", aJob.Operations.__class__.__name__)
-    print("Operations as a bases: ", aJob.Operations.__class__.__bases__)
-    print("Operations as a mro: ", aJob.Operations.__class__.__mro__)
-    print("Operations as a dict ", aJob.Operations.__class__.__dict__)
-    print("Operations as a module: ", aJob.Operations.__class__.__module__)
-
-    
-    # Assuming aJob.Operations is the object you're interested in
-    methods = [method for method in dir(aJob.Operations) if callable(getattr(aJob.Operations, method)) and not method.startswith('__')]
-
-    print("Available methods for aJob.Operations:")
-    for method in methods:
-        print("    ",method)
-
-    # List attributes of aJob.Operations, excluding methods and magic methods
-    attributes = [attr for attr in dir(aJob.Operations) if not callable(getattr(aJob.Operations, attr)) and not attr.startswith('__')]
-
-    print("Available attributes for aJob.Operations:")
-    for attr in attributes:
-        print("    ", attr)
-
-
-    lclList=aJob.Operations.OutList
-    print("aJob.OperationsOutList: ", lclList)
-    print("aJob.OperationsOutList size: ", lclList.__len__())
-
-    if lclList.__len__() > 0:
-        anOp=lclList[0];
-        print("anOp: ", anOp)
-        anOpattributes = [attr for attr in dir(aJob.Operations) if not callable(getattr(aJob.Operations, attr)) and not attr.startswith('__')]
-
-        print("Available attributes for aJob.Operations:")
-        for attr in anOpattributes:
-            print("    ", attr)
-        print("Name", anOp.Name)    
-
-
-
-    parent=aJob.Operations.getParent()
-    print("Parent: ", parent)
-
-    print ("++++++++++++++++++++++++++++++++++++++++++====================")
-    #print("aJob.Operations.Content:", aJob.Operations.Content)
-    print("aJob.Operations.inlist:", aJob.Operations.InList)
-    print ("|||||||||||||||||||||||||")
-    print("aJob.Operations.outlist:", aJob.Operations.OutList)
-    print ("++++++++++++++++++++++++++++++++++++++++++====================")
-    #try:
-    #    iter(aJob.Operations)
-    #    print("aJob.Operations is iterable.")
-    #except TypeError:
-    #    print("aJob.Operations is not iterable.")
-
-   
-    
-    
-    print("OrderOutputBy: ", aJob.OrderOutputBy)
-    print("OutList: ", aJob.OutList)
-    print("OutListRecursive: ", aJob.OutListRecursive)
-    print("Parents: ", aJob.Parents)
-    print("Path: ", aJob.Path)
-    print("Placement: ", aJob.Placement)
-    print("PostProcessor: ", aJob.PostProcessor)
-    print("PostProcessorArgs: ", aJob.PostProcessorArgs)
-    print("PostProcessorOutputFile: ", aJob.PostProcessorOutputFile)
-    print("PropertiesList: ", aJob.PropertiesList)
-    print("Removing: ", aJob.Removing)
-    print("SetupSheet: ", aJob.SetupSheet)
-
-    SetupSheetDir=dir(aJob.SetupSheet)
-    print("SetupSheetDir: ", SetupSheetDir)
-
-    #ret=aJob.SetupSheet.name
-    #type(ret)
-    #print("    output path", ret)
-    
-    print("SetupSheet: ", aJob.SetupSheet)
-    print("SplitOutput: ", aJob.SplitOutput)
-    print("State: ", aJob.State)
-    print("Stock: ", aJob.Stock)
-    print("Tools: ", aJob.Tools)
-    print("Type: ", aJob.JobType)
-    print("TypeId: ", aJob.TypeId)
-    #print(": ", aJob.)
-
-    print("\n\n")
-
-
-    print()
-
-    print("-----------------\n\n")
-
-
-
-
-print("=========================================================================\n")
-print("=========================================================================\n")
-
-
-
-#exit()
 
 print("============================Starting PATH stuff =============================================\n")
 
